dd135431e3d4b5fac522586229e402fa7591bf5d
Kiran Murugulla <Kiran.Murugulla@hotmail.com>
2014-07-22 22:54:15 -0400

    Initial commit

---
+++ bundle/src/main/java/com/aem/samples/HelloService.java
@@ -0,0 +1,13 @@
+package com.aem.samples;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
